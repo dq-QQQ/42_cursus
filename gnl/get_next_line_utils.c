09b668617a33bee715cd58988b147bfa14d28317
Kyu Jin Lee <kjsl4tw@naver.com>
2021-05-23 20:14:07 +0900

    Update get_next_line_utils.c

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -6,81 +6,86 @@
 /*   By: kyujlee <kyujlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 18:17:24 by kyujlee           #+#    #+#             */
-/*   Updated: 2021/05/21 17:04:34 by kyujlee          ###   ########.fr       */
+/*   Updated: 2021/05/23 19:40:20 by kyujlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t        ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    int i;
-    i = 0;
-    if (!s)
-        return (0);
-    while (s[i])
-        i++;
-    return (i);
+	int i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (*(s + i))
+		i++;
+	return (i);
 }
 
-
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    if (!dst && !src)
-        return ((void *)0);
-    while (n-- > 0)
-        *(dst + n) = *((char *)src + n);
-    return (dst);
+	if (!dst && !src)
+		return ((void *)0);
+	while (n-- > 0)
+		*((char *)dst + n) = *((char *)src + n);
+	return (dst);
 }
 
-
-char    *my_strjoin(char const *s1, char const *s2)
+char	*my_strjoin(char const *s1, char const *s2, int *flag)
 {
-    size_t    len_1;
-    size_t    len_2;
-    char    *ret;
+	size_t	len_1;
+	size_t	len_2;
+	char	*ret;
 
-    if (!s1 && !s2)
-        return (0);
-    len_1 = ft_strlen(s1);
-    len_2 = ft_strlen(s2);
-    if ((ret = (char *)malloc(len_1 + len_2 + 1)) == 0)
-        return (0);
-    *(ret + len_1 + len_2) = 0;
-    ft_memcpy(ret, s1, len_1);
-    ft_memcpy(ret + len_1, s2, len_2);
+	if (!s1 && !s2)
+		return (0);
+	len_1 = ft_strlen(s1);
+	len_2 = ft_strlen(s2);
+	if ((ret = (char *)malloc(len_1 + len_2 + 1)) == 0)
+	{
+		*flag = -3;
+		free((char *)s1);
+		return (0);
+	}
+	*(ret + len_1 + len_2) = 0;
+	ft_memcpy(ret, s1, len_1);
+	ft_memcpy(ret + len_1, s2, len_2);
 	free((char *)s1);
-    return (ret);
+	return (ret);
 }
 
-char    *my_strndup(const char *s1, size_t idx)
+char	*my_strndup(const char *s1, size_t idx, int *flag)
 {
-    char    *ret;
-    size_t        len;
-    size_t        i;
+	char	*ret;
+	size_t	len;
+	size_t	i;
 
-    len = idx;
-    i = 0;
-    if ((ret = (char *)malloc(len + 1)) == 0)
-        return (0);
-    *(ret + len) = 0;
+	len = idx;
+	i = 0;
+	if ((ret = (char *)malloc(len + 1)) == 0)
+	{
+		*flag = -2;
+		return (0);
+	}
+	*(ret + len) = 0;
 	ft_memcpy(ret, s1, len);
-    return (ret);
+	return (ret);
 }
 
-size_t    my_strnewline(char *str)
+size_t	my_strnewline(char *str)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    if (!str)
-        return (0);
-    while (str[i])
-    {
-        if (str[i] == '\n')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	if (!str)
+		return (0);
+	while (*(str + i))
+	{
+		if (*(str + i) == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
-
