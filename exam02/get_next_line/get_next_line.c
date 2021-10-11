df58829e5941242834c020f93dbabbbb78653b5f
Kyujin Lee <kyujlee@c8r1s12.42seoul.kr>
2021-10-11 20:06:45 +0900

    test

--- exam02/get_next_line/get_next_line.c
+++ exam02/get_next_line/get_next_line.c
@@ -1,202 +1,85 @@
 #include "get_next_line.h"
 
-typedef struct	s_str
-{
-	char			*content;
-	struct s_str	*next;
-}				t_str;
-
-static int
-	ft_strlen(char const *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+int ft_strlen(char *str){
+	int ret = 0;
+	while (str[ret])
+		ret++;
+	return  ret;
 }
 
-static char
-	*ft_strdup(char const *str)
-{
-	char	*cpy;
-	int		i;
+char *ft_strndup(char *str, int size){
+	char *ret;
+	int i = 0;
 
-	if (!str)
-		return (NULL);
-	if (!(cpy = (char*)malloc(sizeof(*cpy) * (ft_strlen(str) + 1))))
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		cpy[i] = str[i];
+	ret = (char *)malloc(sizeof(char) * size + 1);
+	ret[size] = '\0';
+	while (str[i] && i < size){
+		ret[i] = str[i];
 		i++;
 	}
-	cpy[i] = 0;
-	return (cpy);
+	return ret;
 }
 
-static int
-	ft_strchr(char const *str, char c)
-{
-	int	i;
+char *ft_strjoin(char *str1, char *str2){
+	char *ret;
+	int len1 = ft_strlen(str1);
+	int len2 = ft_strlen(str2);
+	int i = 0,j = 0;
 
-	i = 0;
-	while (str[i])
-		if (str[i++] == c)
-			return (1);
-	return (0);
-}
-
-static t_str
-	*str_add(t_str **str, char *content)
-{
-	t_str	*new;
-	t_str	*tmp;
-
-	if (!content)
-		return (NULL);
-	if (!(new = (t_str*)malloc(sizeof(*new))))
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	if (!*str)
-		*str = new;
-	else
-	{
-		tmp = *str;
-		while ((*str)->next)
-			*str = (*str)->next;
-		(*str)->next = new;
-		*str = tmp;
+	ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	*(ret + len1 +len2) = '\0';
+	while (i < len1){
+		ret[i] = str1[i];
+		i++;
 	}
-	return (new);
-}
 
-static int
-	str_strchr(t_str *str, char c)
-{
-	while (str)
-	{
-		if (ft_strchr(str->content, c))
-			return (1);
-		str = str->next;
+	while (i < len1 + len2){
+		ret[i] = str2[j];
+		i++;
+		j++;
 	}
-	return (0);
+	return ret;
 }
 
-static int
-	str_length(t_str *str)
-{
-	int	total;
-	int	i;
+char *ft_strchr(char *str, char c){
+	int i = 0;
 
-	total = 0;
-	while (str)
-	{
-		i = 0;
-		while (str->content[i] && str->content[i] != '\n')
-			i++;
-		total += i;
-		if (str->content[i] == '\n')
-			return (total);
-		str = str->next;
-	}
-	return (total);
-}
-
-static int
-	str_write(t_str **str, char **line)
-{
-	char	*ltmp;
-	t_str	*tmp;
-	int		length;
-	int		i, j;
-
-	length = str_length(*str);
-	if (!(ltmp = (char*)malloc(sizeof(*str) * (length + 1))))
-		return (0);
-	ltmp[length] = 0;
-	*line = ltmp;
-	j = 0;
-	while (*str)
-	{
-		i = 0;
-		while ((*str)->content[i] && (*str)->content[i] != '\n')
-			(*line)[j++] = (*str)->content[i++];
-		(*line)[j] = 0;
-		if ((*str)->content[i++] == '\n')
-		{
-			j = 0;
-			while ((*str)->content[i])
-				(*str)->content[j++] = (*str)->content[i++];
-			(*str)->content[j] = 0;
-			return (1);
+	while (str[i]){
+		if (str[i] == c){
+			return (str + i);
 		}
-		else
-		{
-			tmp = (*str)->next;
-			free((*str)->content);
-			free(*str);
-			*str = tmp;
+		i++;
+	}
+	if (c == '\0')
+		return(str + i);
+	return NULL;
+}
+
+int get_next_line(char **line){
+	static char *data;
+	char *tmp;
+	int byte;
+	char *nextline;
+	char buff[2] = {0};
+
+	if (data == NULL){
+		data = ft_strndup("", 0);
+		while ((byte = read(0, buff, 1) > 0)){
+			tmp = data;
+			data = ft_strjoin(data, buff);
+			free(tmp);
 		}
 	}
-	return (1);
-}
 
-static int
-	str_clear(t_str **str)
-{
-	t_str	*next;
+	nextline = ft_strchr(data, '\n');
 
-	while (*str)
-	{
-		next = (*str)->next;
-		free((*str)->content);
-		free(*str);
-		*str = next;
+	if (nextline != NULL){
+		*line = ft_strndup(data, nextline - data);
+		data = nextline + 1;
+		return 1;
 	}
-	return (0);
-}
-
-static int
-	read_file(t_str **str)
-{
-	char	buffer[129];
-	int		r;
-	int		total;
-
-	total = 0;
-	while ((r = read(0, buffer, 128)) > 0)
-	{
-		buffer[r] = 0;
-		if (!str_add(str, ft_strdup(buffer)))
-			return (-1);
-		if (ft_strchr(buffer, '\n'))
-			return (1);
+	else{
+		*line = ft_strndup(data, ft_strchr(data, '\0') - data);
+		return 0;
 	}
-	return (total > 0);
 }
-
-int
-	get_next_line(char **line)
-{
-	static t_str	*buffer = NULL;
-	int				read_ret;
-	
-	read_ret = 1;
-	if (!buffer || !str_strchr(buffer, '\n'))
-		read_ret = read_file(&buffer);
-	if (read_ret < 0)
-		return (str_clear(&buffer) | -1);
-	if (!str_write(&buffer, line))
-		return (str_clear(&buffer) | -1);
-	read_ret = 1;
-	if (!buffer || !str_strchr(buffer, '\n'))
-		read_ret = read_file(&buffer);
-	if (read_ret < 0)
-		return (str_clear(&buffer) | -1);
-	return (!!buffer);
-}
-
--- exam02/get_next_line/main.c
+++ exam02/get_next_line/main.c
@@ -1,22 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/11 16:29:19 by ncolomer          #+#    #+#             */
-/*   Updated: 2019/11/14 19:20:21 by ncolomer         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int
-	main(void)
+int main(void)
 {
 	int		r;
 	char	*line;
