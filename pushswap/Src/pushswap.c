e85d235de37d57b65678b648cebac11ede85c273
Kyujin Lee <kyujlee@c8r1s12.42seoul.kr>
2022-01-25 19:52:14 +0900

    ho

--- pushswap/Src/pushswap.c
+++ pushswap/Src/pushswap.c
@@ -6,42 +6,69 @@
 /*   By: kyujlee <kyujlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 15:56:40 by kyujlee           #+#    #+#             */
-/*   Updated: 2022/01/25 19:24:47 by kyujlee          ###   ########.fr       */
+/*   Updated: 2022/01/25 19:52:04 by kyujlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Inc/pushswap.h"
 
 
-t_stack *init_stack(char *str)
-{
-	char *tmp_ptr;
-	char *tmp_str;
-	int i;
-	t_stack *ret;
-	t_stack tmp;
+// t_stack *init_stack(char *str)
+// {
+// 	char *tmp_ptr;
+// 	char *tmp_str;
+// 	int i;
+// 	t_stack *ret;
+// 	t_stack tmp;
 
-	ret = (t_stack *)malloc(sizeof(t_stack));
-	while (str){
-		if (ft_isdigit(*str)){//숫자면 1 아니면 0(숫자면)
-			tmp_ptr = str;
-			while (ft_isdigit(*tmp_ptr))// 숫자 아닐때까지 가서
-				tmp_ptr++;
-			tmp_str = (char *)malloc(tmp_ptr - str + 1);
-			tmp_str[tmp_ptr - str] = '\0';
-			i = 0;
-			while (tmp_ptr != str)//복사
-				tmp_str[i++] = *(str++);
-			tmp.data = ft_atoi(tmp_str);
-			ft_lstadd_back(&ret, &tmp);
-			free(tmp_str);
+// 	ret = (t_stack *)malloc(sizeof(t_stack));
+// 	while (str){
+// 		if (ft_isdigit(*str)){//숫자면 1 아니면 0(숫자면)
+// 			tmp_ptr = str;
+// 			while (ft_isdigit(*tmp_ptr))// 숫자 아닐때까지 가서
+// 				tmp_ptr++;
+// 			tmp_str = (char *)malloc(tmp_ptr - str + 1);
+// 			tmp_str[tmp_ptr - str] = '\0';
+// 			i = 0;
+// 			while (tmp_ptr != str)//복사
+// 				tmp_str[i++] = *(str++);
+// 			tmp.data = ft_atoi(tmp_str);
+// 			ft_lstadd_back(&ret, &tmp);
+// 			free(tmp_str);
+// 		}
+// 		else
+// 			str++;
+// 	}
+// 	return (ret);
+// }
+
+
+
+static	t_stack	*ft_crtstack(char *s, t_stack *st, int u, t_stack *res)
+{
+	t_stack		*buf;
+	int			dig;
+	char		**arr;
+
+	arr = ft_split(s, ' ');
+	while (arr[++u])
+	{
+		dig = ft_atoi(arr[u]);
+		if (!res)
+		{
+			res = (t_stack *)malloc(sizeof(t_stack))
+			buf = res;
 		}
 		else
-			str++;
+		{
+			buf->next = (t_stack *)malloc(sizeof(t_stack))
+			buf = buf->next;
+		}
+		buf->data = dig;
+		buf->next = 0;
 	}
-	return (ret);
+	return (res);
 }
-
 
 int				main(int argc, char **argv)
 {
@@ -52,8 +79,9 @@ int				main(int argc, char **argv)
 	ms.more = 0;
 	ms.cn = 0;
 	st = 0;
-	if (argc < 2)
-		return (0);
-	st = init_stack(argv[1]);
+	if (argc == 2){
+		st = ft_crtstack(argv[1], st, -1, 0);
+	}
 	write (1, "ho", 2);
+	return (0);
 }
--- pushswap/Src/pushswap_utils2.c
+++ pushswap/Src/pushswap_utils2.c
@@ -1,21 +1,89 @@
 #include "../Inc/pushswap.h"
 
-t_stack	*ft_lstlast(t_stack *lst)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	if (!lst)
+	if (!dst && !src)
 		return ((void *)0);
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
+	while (n-- > 0)
+		*((unsigned char *)dst + n) = *((unsigned char *)src + n);
+	return (dst);
 }
 
-void	ft_lstadd_back(t_stack **lst, t_stack *stack)
+static size_t	cnt_word(char const *s, char c)
 {
+	size_t	i;
+	size_t	cnt;
 
-	if (!*lst)
+	i = 0;
+	cnt = 0;
+	while (*(s + i))
 	{
-		*lst = stack;
-		return ;
+		if (*(s + i) == c)
+			i++;
+		else
+		{
+			cnt++;
+			while (*(s + i) != c && *(s + i))
+				i++;
+		}
 	}
-	(ft_lstlast(*lst))->next = stack;
+	return (cnt);
+}
+
+static void		free_malloc(char **ret, size_t word)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < word)
+	{
+		free(*(ret + i));
+		i++;
+	}
+	free(ret);
+}
+
+static int		cut_str(char const *s, char c, char **ret, size_t word)
+{
+	size_t	idx1;
+	size_t	idx2;
+
+	idx1 = 0;
+	while (*(s + idx1))
+	{
+		if (*(s + idx1) == c)
+			idx1++;
+		else
+		{
+			idx2 = 0;
+			while (*(s + idx1 + idx2) != c && *(s + idx1 + idx2))
+				idx2++;
+			if ((*(ret + word) = (char *)malloc(idx2 + 1)) == 0)
+			{
+				free_malloc(ret, word);
+				return (0);
+			}
+			ft_memcpy(*(ret + word), s + idx1, idx2);
+			*(*(ret + word) + idx2) = 0;
+			idx1 += idx2;
+			word++;
+		}
+	}
+	return (1);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	size_t	cnt;
+	char	**ret;
+
+	if (!s)
+		return ((void *)0);
+	cnt = cnt_word(s, c);
+	if ((ret = (char **)malloc(sizeof(char *) * (cnt + 1))) == 0)
+		return (0);
+	*(ret + cnt) = 0;
+	if (!cut_str(s, c, ret, 0))
+		return (0);
+	return (ret);
 }
