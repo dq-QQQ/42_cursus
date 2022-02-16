66ff13c74e047eadf0c52286705ff04765308ae4
Kyujin Lee <kyujlee@c8r1s12.42seoul.kr>
2022-02-16 17:30:50 +0900

    make philo

---
+++ philosophers/Inc/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyujlee <kyujlee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 17:28:25 by kyujlee           #+#    #+#             */
+/*   Updated: 2022/02/16 17:30:35 by kyujlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <sys/time.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+
+
+#endif
